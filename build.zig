376b04afde100e1daaa8d2fe0e3d2fcf1fa68751
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-01-13 14:08:26 -0600

    replace default names for Windows users

--- build.zig
+++ build.zig
@@ -1,18 +1,18 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const exe = b.addExecutable(.{
+    const bin = b.addExecutable(.{
         .name = "tmpNote",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = b.graph.host,
         }),
     });
-    exe.linkLibC();
-    b.installArtifact(exe);
+    bin.linkLibC();
+    b.installArtifact(bin);
     
-    const run_exe = b.addRunArtifact(exe);
+    const run_bin = b.addRunArtifact(bin);
 
     const run_step = b.step("run", "execute the program");
-    run_step.dependOn(&run_exe.step);
+    run_step.dependOn(&run_bin.step);
 }

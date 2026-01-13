8b96da9e705b02edf45b0e9c3902a299b019e36b
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-01-13 11:46:07 -0600

    move source to new dir

---
+++ src/conf.zig
@@ -0,0 +1,177 @@
+const std = @import("std");
+
+const log = @import("helpers.zig").log;
+
+const fs = std.fs;
+const mem = std.mem;
+const fmt = std.fmt;
+const meta = std.meta;
+const ascii = std.ascii;
+
+var stdout_buf:[1024]u8 = undefined;
+var stdout_wr = fs.File.stdout().writer(&stdout_buf);
+const stdout = &stdout_wr.interface;
+
+const valid_byte_sizes = enum {
+    b,
+    kb,
+    mb,
+    gb,
+    tb,
+    pb,
+    eb,
+    yb,
+    any,
+    bad,
+};
+const bool_enum = enum {
+    TRUE,
+    FALSE,
+    T,
+    F,
+    BAD,
+};
+const conf_vals = enum {
+    port,
+    name,
+    max_note_size,
+    escape_html_ampersand,
+    bad,
+};
+
+//custom errors
+const err = error {
+    Invalid_Value,
+    Invalid_Key,
+    Invalid_Line,
+    The_Whole_Damn_Thing,
+};
+
+const def_conf = @embedFile("config");
+
+pub const conf = struct {
+    port: u16,
+    name: []const u8,
+    max_note_size: u64,
+    escape_html_ampersand: bool,
+
+    const Self = @This();
+
+    pub fn read(alloc:mem.Allocator) !Self {
+        var port:u16 = 7855;
+        var name:[]const u8 = "tmpNote";
+        var max_note_size:u64 = 1024 * 1024; //1MB
+        var escape_html_ampersand:bool = true;
+
+        var fi = fs.cwd().openFile("config", .{}) catch |e| {
+            try log.errf("failed to read config {t}", .{e});
+            @panic("failed to fail");
+        }; defer fi.close();
+
+        var fi_buf:[1024]u8 = undefined;
+        var fi_R = fi.reader(&fi_buf);
+        const fi_I = &fi_R.interface;
+
+        var li_N:usize = 0;
+        while (fi_I.takeDelimiter('\n') catch |e| {
+            try log.errf("{t}", .{e});
+            @panic("huh... failed to fail");
+        }) |li| {
+            li_N += 1;
+            var itr = mem.splitSequence(u8, li, " : ");
+            if (itr.next()) |keyR| {
+                if (itr.next()) |val| {
+                    const key = meta.stringToEnum(conf_vals, keyR) orelse conf_vals.bad;
+                    if (keyR.len == 0) conf_err(err.Invalid_Key, li_N, "no key", null);
+                    switch (key) {
+                        .port => port = fmt.parseInt(u16, val, 10) catch |e| {
+                            conf_err(e, li_N, "not a number", val);
+                            continue;
+                        },
+                        .name => name = try alloc.dupe(u8, val),
+                        .max_note_size => {
+                            const v_b_s = valid_byte_sizes;
+
+                            var ext = std.array_list.Managed(u8).init(alloc);
+                            defer ext.deinit();
+
+                            var si_str_arr = std.array_list.Managed(u8).init(alloc);
+                            defer si_str_arr.deinit();
+
+                            for (val) |c| if (ascii.isDigit(c)) {
+                                si_str_arr.append(c) catch |e| try log.errf("{t}", .{e});
+                            } else ext.append(c) catch |e| try log.errf("{t}", .{e});
+
+                            const si_str:[]const u8 = si_str_arr.items;
+
+                            if (si_str.len == 0) conf_err(err.Invalid_Value, li_N, "no number found in", val); 
+                            const si:u64 = fmt.parseInt(u64, si_str, 10) catch |e| {
+                                conf_err(e, li_N, "not a number", si_str);
+                                continue;
+                            };
+
+                            var extL_buf:[1024]u8 = undefined;
+                            const extL = ascii.lowerString(&extL_buf, ext.items);
+                            const v = meta.stringToEnum(v_b_s, extL) orelse v_b_s.bad;
+
+                            var mult_num:usize = 0;
+                            max_note_size = si;
+
+                            switch (v) {
+                                .any => continue,
+                                .b => continue,
+                                .kb => mult_num = 1, 
+                                .mb => mult_num = 2,
+                                .gb => mult_num = 3,
+                                .tb => mult_num = 4,
+                                .pb => mult_num = 5,
+                                .eb => mult_num = 6,
+                                .yb => mult_num = 7,
+                                .bad => conf_err(err.Invalid_Value, li_N, "bad extension", ext.items),
+                            }
+                            for (0..mult_num) |_| max_note_size *= 1024;
+                        },
+                        .escape_html_ampersand => {
+                            var valU_buf:[1024]u8 = undefined;
+                            const valU = ascii.upperString(&valU_buf, val);
+                            const valEnum = meta.stringToEnum(bool_enum, valU) orelse bool_enum.BAD;
+                            switch (valEnum) {
+                                .TRUE =>  escape_html_ampersand = true,
+                                .T => escape_html_ampersand = true,
+                                .F => escape_html_ampersand = false,
+                                .FALSE => escape_html_ampersand = false,
+                                .BAD => conf_err(err.Invalid_Value, li_N, "not a bool", val),
+                            }
+                        },
+                       .bad => conf_err(err.Invalid_Key, li_N, "key doesn't exist", null), 
+                    }
+                } else conf_err(err.Invalid_Line, li_N, "missing key and/or value", null);
+            } else conf_err(err.Invalid_Line, li_N, "it's just bad", null);
+        }
+
+        //make sure everything was flushed;
+        try stdout.flush();
+
+        return Self{
+            .port = port,
+            .name = name,
+            .max_note_size = max_note_size,
+            .escape_html_ampersand = escape_html_ampersand,
+        };
+    }
+};
+
+fn conf_err(
+    e:anyerror,
+    li_N:usize,
+    comptime msg:[]const u8,
+    thing:?[]const u8
+) void {
+    if (thing != null) {
+        log.errf("(conf err on line {d}) {t} : "++msg++" '{s}'",
+                .{li_N, e, thing.?}) catch return;
+    } else {
+        log.errf("(conf err on line {d}) {t} : "++msg,
+                .{li_N, e}) catch return;
+    }
+}
---
+++ src/helpers.zig
@@ -0,0 +1,126 @@
+const std = @import("std");
+
+const crypto = std.crypto;
+const http = std.http;
+const heap = std.heap;
+const fmt = std.fmt;
+const mem = std.mem;
+
+var stdout_buf:[1024]u8 = undefined;
+var stdout_wr = std.fs.File.stdout().writer(&stdout_buf);
+const stdout = &stdout_wr.interface;
+
+pub const send = struct {
+
+    const Self = @This();
+
+    pub fn headers(
+        status:i16,
+        curTime: []u8,
+        req:http.Server.Request
+    ) !void {
+        try Self.headersWithType(status, curTime, req, null);
+    }
+
+    pub fn headersWithType(
+        status:i16,
+        curTime: []u8,
+        req:http.Server.Request,
+        content_type:?[]const u8
+    ) !void {
+        var gpa = heap.GeneralPurposeAllocator(.{}){};
+        defer _ = gpa.deinit();
+        const alloc = gpa.allocator();
+
+        const heads = [_][]const u8 {
+            switch (status) {
+                400 => "HTTP/1.1 400 Bad Request",
+                200 => "HTTP/1.1 200 OK",
+                403 => "HTTP/1.1 403 FORBIDDEN",
+                404 => "HTTP/1.1 404 not found",
+                else => "HTTP/1.1 500 Internal Server Error",
+            },
+            try fmt.allocPrint(alloc, "Content-Type: {s}", .{content_type orelse "text/html"}),
+            "x-content-type-options: nosniff", 
+            "server: homebrew zig http server",
+            try fmt.allocPrint(alloc, "date: {s}", .{curTime}),
+            ""
+        }; defer for ([_]usize{ 1, 4, }) |i| alloc.free(heads[i]);
+
+        for (heads) |h| {
+            req.server.out.print("{s}\r\n", .{h}) catch return;
+            req.server.out.flush() catch return;
+        } 
+    }
+};
+
+pub fn ranStr(len:usize, alloc: mem.Allocator) ![]u8 {
+    const chars:[]const u8 = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPKLJHGFDSAZXCVBNM1234567890";
+
+    var pran = crypto.random;
+    const buf = try alloc.alloc(u8, len);
+    for (buf) |*byte| {
+        const i = pran.intRangeAtMost(usize, 0, chars.len-1);
+        byte.* = chars[i];
+    }
+
+    return buf;
+}
+
+pub const log = struct {
+    pub fn req(curTime:[]const u8, remAddr:[]const u8, reqPage: []const u8) !void {
+        const l = [_][]const u8 {
+            "\x1b[1;37m[\x1b[1;33mreq\x1b[1;37m]:\x1b[0m ",
+            "\x1b[1;36mdate\x1b[1;37m{\x1b[0m",
+            curTime,
+            "\x1b[1;37m}\x1b[0m ",
+            "\x1b[1;35maddr\x1b[1;37m{\x1b[0m",
+            remAddr,
+            "\x1b[1;37m}\x1b[0m ",
+            "\x1b[1;34mpage\x1b[1;37m{\x1b[0m",
+            reqPage,
+            "\x1b[1;37m}\x1b[0m",
+            "\n"
+        };
+        for (l) |p| try stdout.print("{s}", .{p});
+        try stdout.flush();
+    }
+    pub fn err(comptime msg:[]const u8, args:anytype) !void {
+        try stdout.print("\x1b[1;37m[\x1b[1;31merr\x1b[1;37m]:\x1b[0m ", .{});
+        try stdout.print(msg++"\n", args);
+        try stdout.flush();
+    }
+    pub fn errf(comptime msg:[]const u8, args:anytype) !void {
+        try log.err(msg, args);
+        std.process.exit(1);
+    }
+    pub fn info(comptime msg:[]const u8, args:anytype) !void {
+        try stdout.print("\x1b[1;37m[\x1b[1;35minfo\x1b[1;37m]:\x1b[0m ", .{});
+        try stdout.print(msg++"\n", args);
+        try stdout.flush();
+    }
+};
+
+pub fn sanitizeHTML(og:[]const u8, alloc:mem.Allocator, escapeAmper:bool) ![]const u8 {
+    const bad = [_][]const u8{"<", ">", "&", "\"", "'"};
+    var new_note:[]const u8 = og;
+    for (0.., bad) |i, char| {
+        const reChar:[]const u8 = switch (i) {
+            0 => "&lt;",
+            1 => "&gt;",
+            2 => if (escapeAmper) "&amp;" else "&",
+            3 => "&quot;",
+            4 => "&apos;",
+            else => {
+                try log.errf("unknown escape: {s}", .{char});
+                return "";
+            },
+        };
+        const new_si = mem.replacementSize(u8, new_note, char, reChar);
+        const tmp_note = try alloc.alloc(u8, new_si);
+        _ = mem.replace(u8, new_note, char, reChar, tmp_note);
+        new_note = tmp_note;
+    }
+
+    return new_note;
+}

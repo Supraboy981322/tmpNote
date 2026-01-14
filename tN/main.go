b87fd32f47a1672b5a679058b97d2e3eb7573c9b
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-01-14 13:28:36 -0600

    move id printing logic to dedicated func

--- tN/main.go
+++ tN/main.go
@@ -7,7 +7,6 @@ import (
 	"time"
 	"bytes"
 	"errors"
-	"strings"
 	"net/url"
 	"net/http"
 )
@@ -130,29 +129,7 @@ func mkReq() {
 	bod, e := io.ReadAll(resp.Body)
 	if e != nil { erorF("failed to read response body", e) }
 	switch act {
-	 case "set":
-		server_no_proto := strings.Split(server, "://")[1]
-		id := string(bod)
-		l1 := fmt.Sprintf("%s  %s[%sid%s]:%s %s %s",
-				"\033[48;2;16;23;41m", "\033[1;38;2;125;134;177m",
-				"\033[1;38;2;192;153;255m", "\033[1;38;2;125;134;177m",
-				"\033[22;38;2;255;255;255m", id, "\033[0m")
-		l2 := fmt.Sprintf("%s %s[%surl%s]:%s %s/view?id=%s%s %s",
-				"\033[48;2;16;23;41m", "\033[1;38;2;125;134;177m",
-				"\033[1;38;2;255;199;119m", "\033[1;38;2;125;134;177m",
-				"\033[22;38;2;255;255;255m", server_no_proto, "\033[1m",
-				id, "\033[0m")
-		var longest int ; for _, l := range []string{ l1, l2, } {
-			if len_no_esc(l) > longest { longest = len_no_esc(l) }
-		}
-		for i, l := range []string{ l1, l2 } {
-			if len_no_esc(l) < longest {
-				diff := longest-len_no_esc(l)
-				for _ = range diff { l += "\033[48;2;16;23;41m \033[0m" }
-			}
-			switch i { case 0: l1 = l ; case 1: l2 = l }
-		}
-		smart_print("%s\n%s\n", l1, l2)
+	 case "set": print_id(bod)
 	 default:
 		os.Stdout.Write(bod)
 		fmt.Println("")

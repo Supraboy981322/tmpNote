ae7b374a55227cf7d73766f075ef60505f3dba6c
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-01-18 10:40:01 -0600

    more managable

--- src/mimes.zig
+++ src/mimes.zig
@@ -5,31 +5,31 @@ const globs = @import("global_types.zig");
 const log = hlp.log;
 const Mime = globs.Mime;
 
+const list = [_][2][]const u8 {
+    .{ "BM", "BMP" },
+    .{ "MZ", "Windows executable" },
+    .{ "%PDF", "PDF" },
+    .{ "\x89PNG", "PNG" },
+    .{ "\x7fELF", "ELF executable" },
+    .{ "\x50\x4b\x03\x04", "zip" },
+    .{ "\xff\xd8\xff", "jpeg" },
+    .{ "\x47\x49\x46\x38", "GIF" },
+    .{ "SQLite format 3\x00", "SQLite database file" },
+};
+
 pub fn chk_mime(b_s:[]const u8) Mime {
     var is_text:bool = true;
     for (b_s) |b| {
         if (!std.ascii.isAscii(b)) { is_text = false ; break; }
     }
     log.deb("is_text == {}", .{is_text}) catch {};
-    const mime = if (is_text) "text/plain" else "";
-    const list = [_][]const u8 {
-        "BM",
-        "MZ",
-        "%PDF",
-        "\x89PNG",
-        "\x7fELF",
-        "\x50\x4b\x03\x04",
-        "\xff\xd8\xff\xfe0",
-        "GIF87a",
-        "GIF89a",
-        "SQLite format 3\x00",
-    };
-    var idx:usize = 0;
-    for (list) |m| {
-        if (hlp.starts_with(b_s, m)) break;
-        idx += 1;
+    var mime:[]const u8 = if (is_text) "text/plain" else "";
+    for (list) |p| {
+        if (mime.len > 0) break;
+        const mag = p[0];
+        const mim = p[1];
+        if (hlp.starts_with(b_s, mag)) { mime = mim; break; }
     }
-    log.deb("mime idx: {d}", .{idx}) catch {};
     log.deb("mime: {s}", .{mime}) catch {};
     return Mime{
         .is_text = true,

ecae1364e1d1c2bea3d3cc920766f2d458bb9fd4
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-01-30 19:26:25 -0600

    more comments

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "tmpNote";
 
   inputs = {
+    # nixpkgs unstable for latest versions
     pkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    # import Zig overlay
     zig_overlay = {
       url = "github:mitchellh/zig-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,16 +14,22 @@
 
   outputs = { self, nixpkgs, zig_overlay, ... } @ inputs: 
     let
+      # system version (you may need to change this)
       system = "x86_64-linux";
+
+      # the server only compiles on one Zig version 
       zigVersion = "0.15.2";
 
+      # selected Zig package
       zig = zig_overlay.packages.${system}.${zigVersion};
 
+      # add the Zig overlay pkgs
       pkgs = import nixpkgs {
         inherit system;
         overlays = [ zig_overlay.overlays.default ];
       };
     in {
+      # install packages
       devShells.${system}.default = pkgs.mkShell {
         packages = [
           zig
@@ -33,6 +41,7 @@
           pkgs.bash
         ];
 
+        # setup shell
         shellHook = ''
           printf "entering tmpNote nix shell"
 

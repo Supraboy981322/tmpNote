98511d65e19633777fb600a96bca06f5838c14a0
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-02-03 12:16:32 -0600

    comments ; work on logging

--- scripts/amalgamate_web.ts
+++ scripts/amalgamate_web.ts
@@ -1,60 +1,70 @@
 "use strict";
 
+//used to ensure the output dir exists 
 import { mkdir } from "node:fs/promises";
-import { minify } from "html-minifier-terser";
-export {}; 
+import { log } from "./logr.ts";
+//html
+import { minify as minify_html } from "html-minifier-terser";
+export {}; //so I can use async stuff  
 
+//file class
 class Fi {
-  content:string;
-  path:string;
+  content:string; //file content
+  path:string;    //filepath
+  //private so async can be used to create instance
   private constructor(content:string, path:string) {
     this.path = path;
     this.content = content;
   }
+  //mtd to create a new file instance 
   static async read(path:string) {
     const fi = Bun.file(path);
     const content = await fi.text();
     return new Fi(content, path);
   }
+  //mtd to get just the content 
   static async get_content(path:string) {
     const fi = await this.read(path);
     return fi.content;
   }
 };
-
+ 
 const new_note = await Fi.read("src/web/new_note.html");
 const script:string = await Fi.get_content("src/web/script.js");
 const css:string = await Fi.get_content("src/web/style.css");
 
+//insert css and js into the document 
 const re_wr = new HTMLRewriter();
-
 for (const thing of [ "*", "head", "title" ]) re_wr.on(thing, {
-  comments(comment) {
+  comments(comment) { //comments are used as placeholders
     const txt:string = comment.text.trim();
+    var ok:boolean = false;
+    var new_plac:boolean = false;
     switch (txt) {
-     case "style.css":
-      // BUG: duplicate '<style>' element 
- //     comment.remove();
+//     BUG: duplicate '<style>' element 
+//     case "style.css"://ok = true
+//     comment.remove();
 //      comment.replace(`<style>${css}</style>`, { html:true });
-      break;
-     case "script.js":
+//      break;
+     case "script.js": ok = true;
       comment.remove();
       comment.replace(`<script async>${script}</script>`, { html:true });
       break;
-     case "server name":
+     case "server name": ok = true; new_plac = true;
       comment.remove();
       comment.replace("<server-name hidden></server-name>", { html:true });
       break;
-     default:
-      console.log(`skipping comment: ${JSON.stringify(txt)}`);
+     default: //log anything that doesn't match 
+      console.log(`\t\t\x1b[35mskipping comment:\x1b[0m ${JSON.stringify(txt)}`);
     }
+    if (ok) console.log(`\t\t\x1b[36m${(new_plac) ? "temporary_replacement" : "replaced"}:\x1b[0m ${JSON.stringify(txt)}`);
   }
 })
 
 console.log("[\x1b[34mstarting...\x1b[0m]");
 for (const page of [ new_note ]) {
   console.log(`\t\x1b[33mminifying:\x1b[0m ${JSON.stringify(page.path)}`);
-  const html = await minify(page.content, {
+  const html = await minify_html(page.content, {
     removeComments:false,
     caseSensitive:true,
     collapseWhitespace:true,
@@ -65,4 +75,4 @@ for (const page of [ new_note ]) {
   const p = `./src/web_comp/${name}`;
   await Bun.write(p, out); 
 }
-console.log("[\x1b[32mdone\x1b[0m]");
+log.msg(32, 0, "done", null);

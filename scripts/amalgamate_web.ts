cfa37a3c23e8b34f024fd81ac8b1d6a3d63f406b
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-02-03 10:29:18 -0600

    start minifying

--- scripts/amalgamate_web.ts
+++ scripts/amalgamate_web.ts
@@ -1,40 +1,49 @@
 "use strict";
 
 import { mkdir } from "node:fs/promises";
+const { minify } = require("html-minifier-terser");
 export {}; 
 
-class fi {
+class Fi {
   content:string;
   path:string;
-  constructor(path:string) {
+  private constructor(content:string, path:string) {
     this.path = path;
+    this.content = content;
+  }
+  static async read(path:string) {
     const fi = Bun.file(path);
-    this.content = await fi.text();
+    const content = await fi.text();
+    return new Fi(content, path);
+  }
+  static async get_content(path:string) {
+    const fi = await this.read(path);
+    return fi.content;
   }
 };
 
-const read_fi = (path:string) => {
-  const c = new fi(path);
-  return c.content;
-};
-
-const new_note = new fi("src/web/new_note.html");
-const script:string = read_fi("src/web/script.js");
-const css:string = read_fi("src/web/style.css");
+const new_note = await Fi.read("src/web/new_note.html");
+const script:string = await Fi.get_content("src/web/script.js");
+const css:string = await Fi.get_content("src/web/style.css");
 
 const re_wr = new HTMLRewriter();
 
-for (const thing of [ "*", "head" ]) re_wr.on(thing, {
+for (const thing of [ "*", "head", "title" ]) re_wr.on(thing, {
   comments(comment) {
     const txt:string = comment.text.trim();
-    ca: switch (txt) {
+    switch (txt) {
      case "style.css":
-      comment.remove();
-      comment.replace(`<style>${css}</style>`, { html:true });
+      // BUG: duplicate '<style>' element 
+ //     comment.remove();
+//      comment.replace(`<style>${css}</style>`, { html:true });
       break;
      case "script.js":
       comment.remove();
       comment.replace(`<script async>${script}</script>`, { html:true });
+      break;
+     case "server name":
+      comment.remove();
+      comment.replace("<server-name hidden></server-name>", { html:true });
       break;
      default:
       console.log(`skipping comment: ${JSON.stringify(txt)}`);
@@ -43,7 +52,16 @@ for (const thing of [ "*", "head" ]) re_wr.on(thing, {
 })
 
 for (const page of [ new_note ]) {
-  const out = re_wr.transform(page.content);
-  const name = page.path.split("/");
-  console.log(name);
+  const html = await minify(page.content, {
+    removeComments:false,
+    caseSensitive:true,
+    collapseWhitespace:true,
+  });
+  console.log(html);
+  const out = re_wr.transform(html);
+  let name_ = page.path.split("/");
+  const name = name_[name_.length-1];
+  const p = `./src/web_comp/${name}`;
+  await Bun.write(p, out); 
+  console.log(p);
 }

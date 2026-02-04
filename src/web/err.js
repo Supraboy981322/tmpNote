d28c2575bace42347a7986e357ff306497763232
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-02-04 17:44:24 -0600

    cleanup

--- src/web/err.js
+++ src/web/err.js
@@ -1,30 +1,42 @@
+"use strict";
+
+//setup page
 window.onload = async () => {
+  //get url
   const p = window.location.href;
+
+  //display the url on the page 
   document.querySelector(".reqPage").innerText = p;
+
+  //get the err stats
   const err_info = document.getElementById("err_info");
   const err_json = JSON.parse(err_info.innerText);
-  const code = err_json.code;
-  switch (+code) {
-    case 400 | 404:
-      var btn = document.createElement("button");
+
+  //switch on err code
+  switch (+err_json.code) {
+    case 400 | 404: //user error
+      //create a btn to mk a new note 
+      var btn = document.createElement("button"); 
       btn.setAttribute("id", "new_note_btn");
-      btn.onclick = new_note;
       btn.setAttribute("class", "tiny");
       btn.innerText = "new note";
+      btn.onclick = new_note;
+
+      //append as hidden element
       btn.setAttribute("hidden", "");
       document.querySelector(".reqPage").after(btn);
-      (async () => {
-        setTimeout(() => {
-          btn.removeAttribute("hidden");
-        }, 999);
-      })();
-      setTimeout(() => {
-        btn.removeAttribute("class");
-      }, 1000);
+
+      //some timing stuff so the button is un-hidden and fades in perfectly 
+      //  (only tested on FireFox)
+      (async () => setTimeout(btn.removeAttribute("hidden"), 999))();
+      setTimeout(btn.removeAttribute("class"), 1000);
+
       break;
-  } 
+    // TODO: handle other error codes 
+  }
 };
 
+//just moves user to new note page
 function new_note() {
   const o = window.location.origin;
   window.location.assign(`${o}/new`);

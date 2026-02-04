8a79c925046489a8399043ec8ae95b0b458b822f
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-02-04 17:08:32 -0600

    comments ; cleanup

--- src/web/script.js
+++ src/web/script.js
@@ -7,7 +7,9 @@ var note_info = undefined; //may use for more than rendering the page
 (async function() {
   //remove "enable JavaScript" warning
   document.getElementById("js_warn").remove();
-  setTimeout(() => {
+
+  //some timing trickery to get animation timing perfect
+  setTimeout(() => { //(only tested on FireFox)
     document.querySelector(".collapsed").removeAttribute("class");
   }, -1);
   
@@ -149,13 +151,16 @@ function view_from_new(elm) {
 async function copy_id(elm) {
   //save the current class
   let old_class = elm.getAttribute("class");
+  
   //add 'clipboard_copy' to the id element class for new styling
   elm.setAttribute("class", `${old_class}, clipboard_copy`);
   let id = elm.innerText; //get the id
+
   try { //copy to clipboard
     await navigator.clipboard.writeText(id);
   } catch (e) { alert(`couldn't copy to clipboard\n\t${e}`); }
-  //wait 100ms then revert to the old styling by switching back class
+
+  //wait 100ms then revert to the old styling by switching class back
   setTimeout(() => {
     elm.setAttribute("class", old_class);
   }, 100);
@@ -165,13 +170,14 @@ async function copy_id(elm) {
 async function newNote() {
   //api url 
   let url = `${window.location.origin}/api/new`;
+
   //note content
   let n = document.getElementById("note").value;
+  
   //make a POST request to server
   let resp = await fetch(url, {
     method: 'POST',
-    //ask for errs in HTML 
-    headers: {
+    headers: {//ask for errs in HTML 
       'err-html': 'true',
     },
     body: n //use note as body
@@ -179,39 +185,51 @@ async function newNote() {
   if (!resp.ok) {
     //the response will contain an HTML err page
     const bod = await resp.text();
+
     //replace the entire DOM with the err HTML page
     document.open("text/html", "replace");
     document.write(bod); //still 100% supported, and perfectly suits this use-case
     document.close();
+
     //throw err once done
     throw new Error(`err, stat: ${resp.status}\n`);
   }
   //get the id
   let id = await resp.text();
+
   //get the id element
   let idElm = document.querySelector(".note_page_btn");
+
   //change the id for the '#id' css attributes 
   idElm.setAttribute("id", "id");
+
   //put the id in the element
   idElm.innerText = `${id}`;
+
   //change the class so css treats it as a different element
   idElm.setAttribute("class", "id");
+
   //make it copy to clipboard instead of executing this fn again 
   idElm.onclick = () => { return copy_id(idElm) };
+
   //remove the note '<textarea>' element
   document.getElementById("note").remove();
+
   //unhide all the id view elements
   let resView = document.querySelectorAll(".resText");
   for (var i = 0; i < resView.length; i++) {
     //alias for the '<button>'
     const btn = resView[i].querySelector("button")
+
     //only attempt to modify '<button>' if it exists
     if (btn != null) { btn.setAttribute("note_id", id); }
+
     //unhide element
     resView[i].removeAttribute("hidden");
   }
 }
 
+//download a file from a note
 function dl_file(caller) {
   //the stupid "dance" (as they say) that must be done
   //  to download a file programmatically in JS

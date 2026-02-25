7da9439ccdfb68ac1f05b894ed72a0df35e380a3
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-02-25 13:56:51 -0600

    some testing, running into a problem

--- proto/thread_buffer.zig
+++ proto/thread_buffer.zig
@@ -1,11 +1,18 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const t = try std.Thread.spawn(.{}, handler.init, .{});
-    _ = t;
+    try handler.init();
+    const t = try std.Thread.spawn(.{}, handler.start, .{});
+    _ = t.detach();
     std.Thread.sleep(100 * std.time.ns_per_ms);
-    try handler.push("foo");
-    try handler.write();
+    for (0..100) |_| try handler.push("foo");
+}
+
+fn stress() !void {
+    for (0..100) |_| {
+        try handler.push("foo");
+        try handler.write();
+    }
 }
 
 pub const handler = struct {
@@ -16,6 +23,14 @@ pub const handler = struct {
     var alloc:std.mem.Allocator = undefined;
     
     const Self = @This(); 
+
+    pub fn start() !void {
+        try init();
+        while (true) {
+            std.Thread.sleep(500 * std.time.ns_per_ms);
+            if (buffer.items.len > 0) try write();
+        }
+    }
    
     pub fn init() !void {
         var thread_alloc = std.heap.ThreadSafeAllocator{
